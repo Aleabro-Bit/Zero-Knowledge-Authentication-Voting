4ac2c3e0695f9e3026077bfc6950a271fe3fc6da
Alessandro <217746647+Aleabro-Bit@users.noreply.github.com>
2026-04-20 02:30:01 +0200

    Delete conversion.js because obsolete

--- conversion.js
+++
@@ -1,7 +0,0 @@
-// Converts and exadecimal address into an integer
-const myAddress = "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2";
-
-const decimalAddress = BigInt(myAddress).toString(10);
-
-console.log("Address in decimal format:");
-console.log(decimalAddress);
